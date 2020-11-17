52222c27b7116a0c2b8eeea55778cc824b9244e3
deni-hi <69893325+deni-hi@users.noreply.github.com>
2020-11-17 16:23:45 +0100

    Get all columns with mean and std values.

--- run_analysis.R
+++ run_analysis.R
@@ -2,7 +2,6 @@
 
 # STEP 1 - Merge the training and the test sets to create one data set.
 # Read test data, set column names to be like the ones given in features.txt
-# that should result in 561 columns, so each element in a line corresponds to one of these features
 featureData<-read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/features.txt", col.names=c("index", "featName"))
 
 testActivityData<-read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/test/y_test.txt", col.names = "activity")
@@ -24,33 +23,30 @@ mergedData<-rbind(testCombinedData, trainCombinedData)
 
 # STEP 2 - Extract only the measurements on the mean and standard deviation for 
 # each measurement.
-library("dplyr")
-meanAndStandardDev<-select(mergedData, tBodyAcc.mean...X:tBodyAcc.std...Z, activity, subject)
-str(meanAndStandardDev)
+library("dplyr") # needed for 'select'
+library("tidyselect") # needed for 'contains'
+meanAndStandardDev<-select(mergedData, contains(c(".mean", ".sdt", "activity", "subject")))
 
-# STEP 3 - Use descriptive activity names to name the activities in the data set
-# for now, each activity is a number, we want to assign it the names given in
+
+# STEP 3 - Use descriptive activity names to name the activities in the data set.
+# For now, each activity is a number, we want to assign it the names given in
 # activity_labels.txt
 activityLabels<-read.table("./getdata_projectfiles_UCI HAR Dataset/UCI HAR Dataset/activity_labels.txt", col.names=c("index", "label"))
+
 # The index of each activity corresponds to the line they're in,
 # so we can get the label by passing the index.
-translateActivity<-function(x){
-    activityLabels$label[x]
-}
+translateActivity<-function(x){activityLabels$label[x]}
 meanAndStandardDev$activity<-sapply(meanAndStandardDev$activity, translateActivity)
 
 # STEP 4 - Appropriately label the data set with descriptive variable names.
-names(meanAndStandardDev)<-c("meanX","meanY","meanZ",
-                             "stdX","stdY","stdZ", 
-                             "activity", "subject")
+# Already done at the beginning.
 
 # STEP 5 - From the data set in step 4, create a second, independent tidy data 
 # set with the average of each variable for each activity and each subject.
-by_activity <- group_by(meanAndStandardDev, activity, subject)
+by_activityAndSubject <- group_by(meanAndStandardDev, activity, subject)
 
-avgSummary<-summarise(
-    by_activity, 
-    avg_meanX=mean(meanX), avg_meanY=mean(meanY), avg_meanZ=mean(meanZ),
-    avg_stdX=mean(stdX), avg_stdY=mean(stdY), avg_stdZ=mean(stdZ))
+avgSummary<-summarise_all(
+    by_activityAndSubject, 
+    funs(mean))
 
-#write.table(avgSummary, file="avgSummary.txt", row.name=FALSE)
+write.table(avgSummary, file="avgSummary.txt", row.name=FALSE)
